e0a99946e7a8ce093eb01cfb158507b4b3ea0e72
Neharoy0 <neharoyjo@gmail.com>
2025-04-21 21:19:44 +0530

    Update base path for GitHub Pages deployment

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/phishing-detection/',  // Add this line for GitHub Pages
+
   server: {
     hmr: {
       overlay: false, // Disable the error overlay to avoid further issues
